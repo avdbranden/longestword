60449dce170a5b6e08cb5bf406f4ff1b2cdf7b13
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2015-11-23 19:09:50 +0100

    problemn of loop in pages_controller

--- longestword/app/controllers/pages_controller.rb
+++ longestword/app/controllers/pages_controller.rb
@@ -14,7 +14,37 @@ class PagesController < ApplicationController
   end
 
   def score
+    grid = params[:grid].split(" ")
+    start_time = params["start_time"].to_i
+    shot = params[:shot]
+    end_time = Time.now.to_i
+    run_game(shot, grid, start_time, end_time)
+  end
 
-
+  def run_game(shot, grid, start_time, end_time)
+    final_time = (end_time - start_time)
+    shot = shot.upcase
+    p shot
+    p "shot"
+    shot = shot.split("")
+    p shot
+    p "shoooooooooooooot"
+    grid.each do |letter|
+      p letter
+      p "letter"
+      if shot.include?(letter)
+        shot.delete_at(shot.index(letter))
+      else
+        not_in_the_grid = "Not in the grid"
+        score = 0
+      end
+    end
+    p score
+    p "score"
+    @result = {
+      grid: grid,
+      score: score,
+      final_time: final_time,
+    }
   end
 end
